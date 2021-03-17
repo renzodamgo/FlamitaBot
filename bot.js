4a09bc11d2891382619a71ec258819277d36b647
Renzo Damian <renzo.damian.go@.com>
2021-03-17 08:17:37 -0500

    adding ping command

--- bot.js
+++ bot.js
@@ -3,11 +3,23 @@ require('dotenv').config()
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
+
+const prefix = '!';
+
 client.on('ready',()=>{
-    console.log('El bot esta activado')
+    console.log('El bot esta activado!!')
 })
 
 client.on('message',msg =>{
+    if (!msg.content.startsWith(prefix) || msg.author.bot) return;  
+
+    const args = msg.content.slice(prefix.length).split(/ + /); 
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        msg.channel.send('Pong! 🏓');
+    }
+
     if (msg.content === "!mod-me"){
         msg.member.roles.add("821716451823714346")
         //msg.channel.send("Not tagged")
