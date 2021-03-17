1a3fed0c8b959de35698fd59b6a21ca947adc30b
Renzo Damian <renzo.damian.go@.com>
2021-03-17 07:31:37 -0500

    asigna roles

--- bot.js
+++ bot.js
@@ -7,4 +7,12 @@ client.on('ready',()=>{
     console.log('Our Bot is loged on')
 })
 
+client.on('message',msg =>{
+    if (msg.content === "!mod-me"){
+        msg.member.roles.add("821716451823714346")
+        //msg.channel.send("Not tagged")
+        //msg.reply('Pong!')
+    }
+})
+
 client.login(process.env.BOT_TOKEN)
